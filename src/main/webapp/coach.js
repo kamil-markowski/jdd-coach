ab1da028e3c78456ec5fb8a9b6235a10e1d1d14e
kamil-markowski <kamilszmarkowski@gmail.com>
2020-06-26 23:49:26 +0200

    Change html to ftlh add add-user servlet.

--- src/main/webapp/coach.js
+++ src/main/webapp/coach.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
     $("#addthat").click(function(){
         document.getElementById("editiondiv").innerHTML =
-            $('#editiondiv').load('./part/addcoach.html')
+            $('#editiondiv').load('./part/addcoach.ftlh')
     });
 
     $("#home").click(function(){
         document.getElementById("editiondiv").innerHTML =
-            $('#editiondiv').load('./part/welcome.html')
+            $('#editiondiv').load('./part/welcome.ftlh')
     });
 
     $("#del").click(function () {
         $("#editiondiv").find("h3").remove();
     });
 
-    var btnconf = $('#confirm');
-    $(btnconf).prop('disabled', true);
-
-    $('.click').click(function() {
-        if ($(btnconf).prop('disabled')) $(btnconf).prop('disabled', false);
-        else $(btnconf).prop('disabled', true);
-    })
-
-    $(button).click(function() {
-        alert('Enabled!');
-    });
+    // var btnconf = $('#confirm');
+    // $(btnconf).prop('disabled', true);
+    //
+    // $('.click').click(function() {
+    //     if ($(btnconf).prop('disabled')) $(btnconf).prop('disabled', false);
+    //     else $(btnconf).prop('disabled', true);
+    // })
+    //
+    // $(button).click(function() {
+    //     alert('Enabled!');
+    // });
 
 });
