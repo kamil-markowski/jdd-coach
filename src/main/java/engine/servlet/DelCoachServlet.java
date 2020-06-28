76d76131477217a79f71500325b406c1f7e8afcc
kamil-markowski <kamilszmarkowski@gmail.com>
2020-06-28 23:47:32 +0200

    Delete option working, clean js.

--- src/main/java/engine/servlet/DelCoachServlet.java
+++ src/main/java/engine/servlet/DelCoachServlet.java
@@ -11,20 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/del-coach")
+@WebServlet("/delete-coach")
 public class DelCoachServlet extends HttpServlet {
 
     @Inject
     CoachService coachService;
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException, ServletException {
 
         resp.setContentType("text/html; charset=UTF-8");
         req.setCharacterEncoding("UTF-8");
         String idParam = req.getParameter("coach");
         Long coachId = Long.parseLong(idParam);
+
         coachService.deleteById(coachId);
 
         resp.sendRedirect("/list-coach");
